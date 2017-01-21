e54c2ebc5aa078695eff2ac986b1726441413bd5
Alexander Sosa <alexander.sosa1993@gmail.com>
2017-01-20 22:05:27 -0800

    Refined and finalized word sifting algorithm.

--- fo4.rb
+++ fo4.rb
@@ -1,87 +1,67 @@
-table = Hash.new
-possible_passwords = Array.new
-incorrect_passwords = Array.new
-new_incorrect_passwords = Array.new
+clear = Proc.new {system "clear"}
 
-clear = Proc.new {system "cls"}
+def likeness(first, second)
+	likeness = 0
 
-word_input = Proc.new {
-	puts "Please enter all words and their likeness..."
-	
-	print "Word: "
-	word = gets.chomp
-	
-	print "Likeness: "
-	likeness = gets.chomp
-	
-	if likeness.size > 0
-		table[word] = likeness
-	else
-		table[word] = "???"
-	end
-
-	clear.call
-}
-
-compare_zero_keys = Proc.new {
-
-	table.each_pair do |word, likeness|
-		if likeness == "0"
-			incorrect_passwords.push(word)
-		else
-			possible_passwords.push(word)
+	first.each_char.with_index(0) do |c, i|
+		if c == second[i]
+			likeness += 1
 		end
 	end
 
-	incorrect_passwords.each do |incorrect|
-		possible_passwords.each do |possible|
+	return likeness
+end
 
-			puts "Comparing: \"#{incorrect}\" to \"#{possible}\""
-			hit = false
-			
-			incorrect.each_char.with_index(0) do |c, i|
-				if possible[i] == c
-					puts "#{incorrect} hit #{possible} at index[#{i}]"
-					hit = true
-				end
-			end
+# Takes in a word and it's likeness. Any words that aren't equally alike are
+# removed from the array of strings.
+def sift(word, likeness, list)
+	alike = 0
+	not_alike = Array.new
 
-			if hit == true
-				new_incorrect_passwords.push(possible)
-				hit = false
+	list.each do |comparing|		
+		word.each_char.with_index(0) do |c, i|
+			if c == comparing[i]
+				alike += 1
 			end
 		end
-		puts
+
+		if alike != likeness
+			not_alike.push(comparing)
+		end
+
+		alike = 0
 	end
-}
+
+	list -= not_alike
+	return list
+end
 
 
-
+# Prompt
 clear.call
-12.times {word_input.call}
+print "Enter all words separated by spaces: "
 
-print "Possible: ", possible_passwords, "\n"
-print "Not Possible: ", incorrect_passwords, "\n\n"
-
-compare_zero_keys.call
-
-possible_passwords -= new_incorrect_passwords
-incorrect_passwords += new_incorrect_passwords
-new_incorrect_passwords = []
-
-print "Possible: ", possible_passwords, "\n"
-print "Not Possible: ", incorrect_passwords, "\n"
+$possible_passwords = gets.chomp.split
 
 
-=begin
-Note:
-	Possible: ["unicorn", "saxhorn"]
-	Not Possible: ["outwile", "bagworm", "outrode", "rubdown", "outlook", "outcook", "outrank", "subjoin", "outpity", "outgave"]
-	
+# Main Loop
+while $possible_passwords.size > 1
+	clear.call
+	puts "Possible Passwords: #{$possible_passwords}"
 
-	Here, we have two possible answers after comparing against zero keys. We know that "bagworm" has a likeness of 2 with the actual apssword.
+	print "Select a word: "
+	choice = gets.chomp
 
-	Now that we've compared all the incorrect_passwords to the possible_passwords, we can check the other way around to eliminate remaining passwords.
-	
-	"saxhorn" has a likeness of 3 with "bagworm" making it an incorrect_password since "bagworm" has a likeness of 2 with the actual password.
-=end
+	if choice == "done"
+		exit(0)
+	end
+
+	print "Likeness: "
+	likeness = gets.chomp.to_i
+
+	$possible_passwords = sift(choice, likeness, $possible_passwords)
+	clear.call
+end
+
+# Display answer
+puts "Success! Password: #{$possible_passwords}"
