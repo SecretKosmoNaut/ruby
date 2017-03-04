9db6de03add272805121abf89aa1109ced538ab0
SecretKosmoNaut <alexander.sosa1993@gmail.com>
2017-03-03 16:53:26 -0800

    Add files via upload

---
+++ lrthw/ex39.rb
@@ -0,0 +1,43 @@
+# create a mapping of state to abbreviation
+states = {
+	'Oregon' => 'OR',
+	'Florida' => 'FL',
+	'California' => 'CA',
+	'New York' => 'NY',
+	'Michigan' => 'MI'
+}
+
+# create a basic set of states and some cities in them
+cities = {
+	'CA' => 'San Francisco',
+	'MI' => 'Detroit',
+	'FL' => 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# puts out some cities
+puts '-' * 10
+puts "NY State has: #{cities['NY']}"
+puts "OR State has: #{cities['OR']}"
+
+# do it by using the state then cities dict
+puts '-' * 10
+puts "Michigan has: #{cities[states['Michigan']]}"
+puts "Florida has: #{cities[states['Florida']]}"
+
+# puts every state abbreviation
+puts '-' * 10
+states.each do |state, abbrev|
+	puts "#{state} is abbreviated #{abbrev}."
+end
+
+#puts every city in state
+puts '-' * 10
+cities.each do |abbrev, city|
+	puts "#{abbrev} has #{city}."
+end
+
+# now do both at the same time
